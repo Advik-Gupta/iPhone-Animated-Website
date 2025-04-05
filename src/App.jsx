5e5b362ba5358865218bbd0b4661249906bc3556
Advik Gupta <advikgupta@Adviks-MacBook-Air.local>
2025-04-05 23:15:34 +0530

    created 3d model section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/navbar/navbar.component";
 import Highlights from "./components/highlights/highlights.component";
 import Hero from "./components/hero/hero.component";
+import Model from "./components/3d-model/model.component";
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <Highlights />
+      <Model />
     </main>
   );
 };
